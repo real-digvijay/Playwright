eee513e6bef2fc8943bf9e3fef7199e08e30d545
Digvijay Kumbhar <digvijaykumbhar22@gmail.com>
2026-06-24 18:07:33 +0530

    addgin new project

---
+++ tests/FirstTest.spec.ts
@@ -0,0 +1,9 @@
+
+
+const {test, page} = require ('@playwright/test');
+
+test('Verify page title', async ({ page }) => { 
+    await page.goto('https://example.com'); 
+    const title = await page.title(); 
+    expect(title).toHaveURL('Example Domain'); 
+  })
