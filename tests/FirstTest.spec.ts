aed592eb2c3b0e9c1977a37ccdfbf33c7ab836de
Digvijay Kumbhar <digvijaykumbhar22@gmail.com>
2026-06-26 14:56:54 +0530

    DemoFile contains all file downloaded fom UDeyme

--- tests/FirstTest.spec.ts
+++ tests/FirstTest.spec.ts
@@ -1,9 +1,12 @@
 
+// import{test, expect} from "@playwright/test"
 
-const {test, page} = require ('@playwright/test');
+// const {test, page} = require ('@playwright/test');
 
-test('Verify page title', async ({ page }) => { 
-    await page.goto('https://example.com'); 
-    const title = await page.title(); 
-    expect(title).toHaveURL('Example Domain'); 
-  })
+// test('Verify page title', async ({ page }) => { 
+//     await page.goto('https://example.com'); 
+//     const title = await page.title(); 
+//     expect(title).toHaveURL('Example Domain'); 
+//   })
+
+
